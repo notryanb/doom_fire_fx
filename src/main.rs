06a8f002ae851edb2f690ba6c0350089b98ecc63
Ryan Blecher <notryanb@gmail.com>
2019-01-15 10:22:21 -0500

    investigate why  Rect doesn't take up whole window?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-extern crate sdl2;
 extern crate rand;
+extern crate sdl2;
 
-use rand::{Rng};
+use rand::Rng;
 use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::{Point, Rect};
@@ -17,47 +17,49 @@ const FIRE_HEIGHT: u32 = 240;
 
 fn main() {
     let color_palette = [
-                (0x07,0x07,0x07),
-                (0x1F,0x07,0x07),
-                (0x2F,0x0F,0x07),
-                (0x47,0x0F,0x07),
-                (0x57,0x17,0x07),
-                (0x67,0x1F,0x07),
-                (0x77,0x1F,0x07),
-                (0x8F,0x27,0x07),
-                (0x9F,0x2F,0x07),
-                (0xAF,0x3F,0x07),
-                (0xBF,0x47,0x07),
-                (0xC7,0x47,0x07),
-                (0xDF,0x4F,0x07),
-                (0xDF,0x57,0x07),
-                (0xDF,0x57,0x07),
-                (0xD7,0x5F,0x07),
-                (0xD7,0x5F,0x07),
-                (0xD7,0x67,0x0F),
-                (0xCF,0x6F,0x0F),
-                (0xCF,0x77,0x0F),
-                (0xCF,0x7F,0x0F),
-                (0xCF,0x87,0x17),
-                (0xC7,0x87,0x17),
-                (0xC7,0x8F,0x17),
-                (0xC7,0x97,0x1F),
-                (0xBF,0x9F,0x1F),
-                (0xBF,0x9F,0x1F),
-                (0xBF,0xA7,0x27),
-                (0xBF,0xA7,0x27),
-                (0xBF,0xAF,0x2F),
-                (0xB7,0xAF,0x2F),
-                (0xB7,0xB7,0x2F),
-                (0xB7,0xB7,0x37),
-                (0xCF,0xCF,0x6F),
-                (0xDF,0xDF,0x9F),
-                (0xEF,0xEF,0xC7),
-                (0xFF,0xFF,0xFF)
-            ];
+        (0x07, 0x07, 0x07),
+        (0x1F, 0x07, 0x07),
+        (0x2F, 0x0F, 0x07),
+        (0x47, 0x0F, 0x07),
+        (0x57, 0x17, 0x07),
+        (0x67, 0x1F, 0x07),
+        (0x77, 0x1F, 0x07),
+        (0x8F, 0x27, 0x07),
+        (0x9F, 0x2F, 0x07),
+        (0xAF, 0x3F, 0x07),
+        (0xBF, 0x47, 0x07),
+        (0xC7, 0x47, 0x07),
+        (0xDF, 0x4F, 0x07),
+        (0xDF, 0x57, 0x07),
+        (0xDF, 0x57, 0x07),
+        (0xD7, 0x5F, 0x07),
+        (0xD7, 0x5F, 0x07),
+        (0xD7, 0x67, 0x0F),
+        (0xCF, 0x6F, 0x0F),
+        (0xCF, 0x77, 0x0F),
+        (0xCF, 0x7F, 0x0F),
+        (0xCF, 0x87, 0x17),
+        (0xC7, 0x87, 0x17),
+        (0xC7, 0x8F, 0x17),
+        (0xC7, 0x97, 0x1F),
+        (0xBF, 0x9F, 0x1F),
+        (0xBF, 0x9F, 0x1F),
+        (0xBF, 0xA7, 0x27),
+        (0xBF, 0xA7, 0x27),
+        (0xBF, 0xAF, 0x2F),
+        (0xB7, 0xAF, 0x2F),
+        (0xB7, 0xB7, 0x2F),
+        (0xB7, 0xB7, 0x37),
+        (0xCF, 0xCF, 0x6F),
+        (0xDF, 0xDF, 0x9F),
+        (0xEF, 0xEF, 0xC7),
+        (0xFF, 0xFF, 0xFF),
+    ];
 
     // Create the pixel buffer
     let mut fire_pixels: Vec<u32> = Vec::with_capacity((FIRE_WIDTH * FIRE_HEIGHT) as usize);
+
+    // Set all pixels to black
     for _ in 0..fire_pixels.capacity() {
         fire_pixels.push(0);
     }
@@ -67,18 +69,20 @@ fn main() {
         let bottom_x_y = ((FIRE_HEIGHT - 1) * FIRE_WIDTH + i) as usize;
         fire_pixels[bottom_x_y] = 36;
     }
-    
+
     // Set Up SDL Windox & Canvas
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("Doom Fire FX", FIRE_WIDTH, FIRE_HEIGHT)
+    let window = video_subsystem
+        .window("Doom Fire FX", FIRE_WIDTH, FIRE_HEIGHT)
         .position_centered()
         .opengl()
         .build()
         .unwrap();
 
-    let mut canvas = window.into_canvas()
+    let mut canvas = window
+        .into_canvas()
         .target_texture()
         .present_vsync()
         .build()
@@ -92,86 +96,55 @@ fn main() {
         .unwrap();
 
     canvas.clear();
-    canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
     canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
-    // let mut frame: u32 = 0;
     'running: loop {
         canvas.clear();
 
         for event in event_pump.poll_iter() {
             match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
                 _ => {}
             }
         }
 
-        fire_texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-            calculate_fire(&mut fire_pixels);
-            let pixel_vec = convert_to_pixel(&fire_pixels, &color_palette);
+        fire_texture
+            .with_lock(None, |buffer: &mut [u8], pitch: usize| {
+                calculate_fire(&mut fire_pixels);
+                let pixel_vec = convert_to_pixel(&fire_pixels, &color_palette);
 
-            for y in 0..FIRE_HEIGHT {
-                for x in 0..FIRE_WIDTH {
-                    let pixel_index = (y * FIRE_HEIGHT + x) as usize;
-                    let pixel = pixel_vec[pixel_index];
+                for y in 0..FIRE_HEIGHT {
+                    for x in 0..FIRE_WIDTH {
+                        let pixel_index = (y * FIRE_HEIGHT + x) as usize;
+                        let pixel = pixel_vec[pixel_index];
 
-                    // let red = rand::thread_rng().gen_range(0, 256) as u8;
-                    // let green = rand::thread_rng().gen_range(0, 256) as u8;
-                    // let blue = rand::thread_rng().gen_range(0, 256) as u8;
-
-                    let offset = ((y * FIRE_WIDTH) + x) as usize;;
-                    buffer[offset] = pixel.red as u8;
-                    buffer[offset + 1] = pixel.blue as u8;
-                    buffer[offset + 2] = pixel.green as u8;
-
+                        let offset = ((y * FIRE_WIDTH) + x) as usize;
+                        buffer[offset] = pixel.red as u8;
+                        buffer[offset + 1] = pixel.blue as u8;
+                        buffer[offset + 2] = pixel.green as u8;
+                    }
                 }
-            }
-            
-        }).unwrap();
+            })
+            .unwrap();
 
-        canvas.copy(&fire_texture, None, Some(Rect::new(0, 0, FIRE_WIDTH, FIRE_HEIGHT)));
+        let rect = Rect::new(0, 0, FIRE_WIDTH * 2, FIRE_HEIGHT * 2);
 
-        // calculate_fire(&mut fire_pixels);
-        // let pixel_vec = convert_to_pixel(&fire_pixels, &color_palette);
+        println!(
+            "Top: {:?}, Left: {:?}, BTM: {:?}, RGHT: {:?}",
+            &rect.top(),
+            &rect.left(),
+            &rect.bottom(),
+            &rect.right()
+        );
 
-        // canvas.with_texture_canvas(&mut fire_texture, |texture_canvas| {
-        //     texture_canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
-        //     let red = rand::thread_rng().gen_range(0, 256) as u8;
-        //     let green = rand::thread_rng().gen_range(0, 256) as u8;
-        //     let blue = rand::thread_rng().gen_range(0, 256) as u8;
-
-        //     texture_canvas.set_draw_color(Color::RGBA(red, green, blue, 255));
-        //     texture_canvas.draw_point(Point::new(100, 100)).expect("Could not draw point");
-
-            // for x in 0..FIRE_WIDTH {
-            //     for y in 0..FIRE_HEIGHT {
-
-                    // texture_canvas.set_draw_color(Color::RGBA(red, green, blue, 255));
-                    // texture_canvas.draw_point(Point::new(x as i32, y as i32)).expect("Could not draw point");
-                    // texture_canvas.clear();
-                    // texture_canvas.present();
-
-                    // let pixel_index = (y * FIRE_HEIGHT + x) as usize;
-                    // let pixel = pixel_vec[pixel_index];
-
-                    // println!("{:?}", pixel);
-
-                    // texture_canvas.set_draw_color(
-                    //     Color::RGBA(
-                    //         pixel.red as u8,
-                    //         pixel.blue as u8,
-                    //         pixel.green as u8,
-                    //         255));
-                    // texture_canvas.draw_point(Point::new(x as i32, y as i32)).unwrap();
-            //     }
-            // }
-        // }).unwrap();
-
+        canvas.copy(&fire_texture, None, Some(rect)).unwrap();
         canvas.present();
     }
 }
@@ -186,9 +159,7 @@ pub struct Pixel {
 
 impl Pixel {
     pub fn is_black(self) -> bool {
-        self.red == 0x07 &&
-        self.green == 0x07 && 
-        self.blue == 0x07
+        self.red == 0x07 && self.green == 0x07 && self.blue == 0x07
     }
 }
 
@@ -235,8 +206,7 @@ pub fn convert_to_pixel(pixel_buffer: &Vec<u32>, color_palette: &[(u32, u32, u32
 
             if pixel.is_black() {
                 pixel.alpha = 0;
-            }
-            else {
+            } else {
                 pixel.alpha = 255;
             }
 
